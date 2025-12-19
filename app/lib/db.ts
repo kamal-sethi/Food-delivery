7dbcdd50dc17e2e8945efb693f23ad1b207fd20a
kamal-sethi <kamalsethi018@gmail.com>
2025-12-19 16:43:06 +0530

    initialized the next app and made a db connection using mongoose

---
+++ app/lib/db.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL || "";
+if (!MONGODB_URL) {
+  throw new Error("db error");
+}
+
+let cached=global.mongoose;
+if(!cached){
+    cached=global.mongoose={conn:null,promise:null}; 
+}
+
+const connectDb=async()=>{
+    if(cached.conn){
+        return cached.conn;
+    }
+    if(!cached.promise){
+        cached.promise=mongoose.connect(MONGODB_URL).then((mongoose)=>
+            mongoose.connection
+        )
+    }
+    try {
+        const conn=await cached.promise;
+        return conn;
+    } catch (error) {
+        console.log(error)
+    }
+}
