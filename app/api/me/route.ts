0ba4761164bf7e9cb89152c6078df980f67c2a31
kamal-sethi <kamalsethi018@gmail.com>
2026-01-05 13:56:53 +0530

    implemented a api for getting the user data from database

---
+++ app/api/me/route.ts
@@ -0,0 +1,31 @@
+import { auth } from "@/app/auth";
+import User from "@/app/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const session = await auth();
+
+    if (!session || !session.user) {
+      return NextResponse.json(
+        { message: "user is not authenticated" },
+        { status: 400 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email }).select(
+      "-password"
+    );
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 400 });
+    }
+
+    return NextResponse.json(user, { status: 400 });
+  } catch (error) {
+    console.error("error", error);
+    return NextResponse.json(
+      { message: "Error in finding the user" },
+      { status: 500 }
+    );
+  }
+}
