8bde5838307ff8744297e132214c4e36f523451d
kamal-sethi <kamalsethi018@gmail.com>
2026-01-20 16:31:01 +0530

    implemeted the order model

---
+++ app/models/order.model.ts
@@ -0,0 +1,85 @@
+import mongoose from "mongoose";
+
+interface IOrder {
+  _id?: mongoose.Types.ObjectId;
+  user: mongoose.Types.ObjectId;
+  items: [
+    {
+      grocery: mongoose.Types.ObjectId;
+      name: string;
+      price: string;
+      unit: string;
+      image: string;
+      quantity: number;
+    },
+  ];
+  totalAmount: string;
+  paymentMethod: "cod" | "online";
+  address: {
+    fullName: string;
+    city: string;
+    state: string;
+    pinCode: string;
+    fullAddress: string;
+    mobile: string;
+    latitude: number;
+    longitude: number;
+  };
+  status:
+    | "pending"
+    | "confirmed"
+    | "out for delivery"
+    | "delivered"
+    | "cancelled";
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const orderSchema = new mongoose.Schema<IOrder>(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    items: [
+      {
+        grocery: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Grocery",
+          required: true,
+        },
+        name: { type: String, required: true },
+        price: { type: String, required: true },
+        unit: { type: String, required: true },
+        image: { type: String, required: true },
+        quantity: { type: Number, required: true },
+      },
+    ],
+    totalAmount: { type: String, required: true },
+    paymentMethod: { type: String, enum: ["cod", "online"], required: true },
+    address: {
+      fullName: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+      pinCode: { type: String, required: true },
+      fullAddress: { type: String, required: true },
+      mobile: { type: String, required: true },
+      latitude: { type: Number, required: true },
+      longitude: { type: Number, required: true },
+    },
+    status: {
+      type: String,
+      enum: [
+        "pending",
+        "confirmed",
+        "out for delivery",
+        "delivered",
+        "cancelled",
+      ],
+      default: "pending",
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Order = mongoose.models.Order || mongoose.model("Order", orderSchema);
+
+export default Order;
